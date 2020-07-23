6d46ae99e634ce50930d9c3686892ef9c5795467
AidanF86 <aidan.t.foley@gmail.com>
2020-07-23 06:18:39 -0400

    Added swallow patch.

--- config.def.h
+++ config.def.h
@@ -6,6 +6,7 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx	    = 20;	/* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -15,10 +16,11 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_whiteblue[]     = "#0088AA";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_whiteblue  },
 };
 
 /* tagging */
@@ -31,9 +33,12 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-    { "St",       NULL,       "Calculator", 0,          1,           -1 },
+	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+    { "St",      NULL,     "Calculator",   0,         1,          1,           0         -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+
 };
 
 /* layout(s) */
@@ -66,59 +71,63 @@ static const char *termcmd[]  = { "st", NULL };
 static const char *firefox[] = { "firefox", NULL };
 static const char *discord[] = { "discord", NULL };
 static const char *calculator[] = { "st", "-t", "Calculator", "-f", "monospace:size=18", "-g", "60x30+520+150", "bc", "-q", NULL };
-static const char *inc_volume[] = { "amixer", "set", "Master", "2%+", "&&", "pkill", "-RTMIN+10", "dwmblocks", NULL };
-static const char *dec_volume[] = { "amixer", "set", "Master", "2%-", "&&", "pkill", "-RTMIN+10", "dwmblocks", NULL };
 static const char *i3_lock[] = { "lockbg", NULL };
 static const char *suspend[] = { "lockbg", "-s", NULL };
+static const char *wallp_menu[] = {"wallp", "-d", NULL };
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,	        	XK_b,	   spawn, 	   {.v = firefox} },
-	{ MODKEY|ShiftMask,     		XK_d,	   spawn,	   {.v = discord} },
-    { MODKEY|ShiftMask,	         	XK_x,	   spawn,	   {.v = i3_lock} },
-    { MODKEY|ShiftMask,             XK_s,      spawn,      {.v = suspend} },
-	{ MODKEY|ShiftMask,	        	XK_a,	   spawn,	   {.v = calculator} },
-    { 0,		 XF86XK_AudioRaiseVolume,      spawn,	   {.v = inc_volume} }, 
-    { 0,		 XF86XK_AudioLowerVolume,      spawn,	   {.v = dec_volume} }, 
+	/* modifier                     key                              function           argument */
+	{ MODKEY,                       XK_p,                            spawn,             {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return,                       spawn,             {.v = termcmd } },
+	{ MODKEY|ShiftMask,	        	XK_b,	                         spawn,             {.v = firefox} },
+	{ MODKEY|ShiftMask,     		XK_d,	                         spawn,             {.v = discord} },
+    { MODKEY|ShiftMask,	         	XK_x,	                         spawn,             {.v = i3_lock} },
+    { MODKEY|ShiftMask,             XK_s,                            spawn,             {.v = suspend} },
+	{ MODKEY|ShiftMask,	        	XK_a,	                         spawn,             {.v = calculator} },
+    { 0,		                    XF86XK_AudioRaiseVolume,         spawn,             SHCMD("amixer set Master 2%+;  pkill -RTMIN+10 dwmblocks") }, 
+    { 0,		                    XF86XK_AudioLowerVolume,         spawn,             SHCMD("amixer set Master 2%-;  pkill -RTMIN+10 dwmblocks") }, 
+    { MODKEY|ShiftMask,             XK_w,                            spawn,             {.v = wallp_menu} },
+    { 0,                            XF86XK_AudioPlay,                spawn,             SHCMD("mpc toggle") },
+    { 0,                            XF86XK_AudioPrev,                spawn,             SHCMD("mpc prev") },
+    { 0,                            XF86XK_AudioNext,                spawn,             SHCMD("mpc next") },
+    { 0,                            XF86XK_AudioStop,                spawn,             SHCMD("mpc stop") },
 
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_f,  togglefullscr, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY, 			XK_minus,  setgaps,	   {.i = -10 } },
-	{ MODKEY, 			XK_equal,  setgaps,	   {.i = +10 } },
-	{ MODKEY|ShiftMask, 		XK_equal,  setgaps,	   {.i = 0 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,                       XK_b,                            togglebar,         {0} },
+	{ MODKEY,                       XK_b,                            togglebar,         {0} },
+	{ MODKEY,                       XK_j,                            focusstack,        {.i = +1 } },
+	{ MODKEY,                       XK_k,                            focusstack,        {.i = -1 } },
+	{ MODKEY,                       XK_i,                            incnmaster,        {.i = +1 } },
+	{ MODKEY,                       XK_d,                            incnmaster,        {.i = -1 } },
+	{ MODKEY,                       XK_h,                            setmfact,          {.f = -0.05} },
+	{ MODKEY,                       XK_l,                            setmfact,          {.f = +0.05} },
+	{ MODKEY,                       XK_Return,                       zoom,              {0} },
+	{ MODKEY,                       XK_Tab,                          view,              {0} },
+	{ MODKEY|ShiftMask,             XK_c,                            killclient,        {0} },
+	{ MODKEY,                       XK_t,                            setlayout,         {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,                            setlayout,         {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,                            setlayout,         {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,                        setlayout,         {0} },
+	{ MODKEY|ShiftMask,             XK_space,                        togglefloating,    {0} },
+	{ MODKEY|ShiftMask,             XK_f,                            togglefullscr,     {0} },
+	{ MODKEY,                       XK_0,                            view,              {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,                            tag,               {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,                        focusmon,          {.i = -1 } },
+	{ MODKEY,                       XK_period,                       focusmon,          {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,                        tagmon,            {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,                       tagmon,            {.i = +1 } },
+	{ MODKEY, 			            XK_minus,                        setgaps,	        {.i = -10 } },
+	{ MODKEY, 			            XK_equal,                        setgaps,	        {.i = +10 } },
+	{ MODKEY|ShiftMask, 		    XK_equal,                        setgaps,	        {.i = 0 } },
+	TAGKEYS(                        XK_1,                                                0)
+	TAGKEYS(                        XK_2,                                                1)
+	TAGKEYS(                        XK_3,                                                2)
+	TAGKEYS(                        XK_4,                                                3)
+	TAGKEYS(                        XK_5,                                                4)
+	TAGKEYS(                        XK_6,                                                5)
+	TAGKEYS(                        XK_7,                                                6)
+	TAGKEYS(                        XK_8,                                                7)
+	TAGKEYS(                        XK_9,                                                8)
+	{ MODKEY|ShiftMask,             XK_q,                            quit,              {0} },
 };
 
 /* button definitions */
@@ -137,4 +146,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
--- config.h
+++ config.h
@@ -6,6 +6,7 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx	    = 20;	/* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -32,9 +33,12 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-    { "St",       NULL,       "Calculator", 0,          1,           -1 },
+	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+    { "St",      NULL,     "Calculator",   0,         1,          1,           0         -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+
 };
 
 /* layout(s) */
@@ -142,4 +146,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
